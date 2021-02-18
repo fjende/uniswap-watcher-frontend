aba39d7bf95654928cdf2a1009e4e8abd050ad62
fjende <rera2k12@gmail.com>
2021-02-18 21:57:20 +0100

    component setup

---
+++ src/components/TokenCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TokenCard() {
+    return (
+        <div>
+            TokenName
+        </div>
+    )
+}
---
+++ src/components/TokenList.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import TokenCard from './TokenCard'
+
+export default function TokenList() {
+    return (
+        <div>
+            <TokenCard/>
+        </div>
+    )
+}
